bdd834b4f0e970fdb84d94e2c3dbec83af3b84eb
shashwat <64552189+electramite@users.noreply.github.com>
2021-02-11 17:33:11 +0530

    Add files via upload

---
+++ eyes_only.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, img = cap.read()
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	eyes = eye_cascade.detectMultiScale(gray)
+	for (ex,ey,ew,eh) in eyes:
+		cv2.rectangle(gray,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+	cv2.imshow('img',gray)
+	h = str(type(eyes))
+	if h == "<class 'tuple'>":
+		print("open your eyes")
+	k = cv2.waitKey(30) & 0xff
+	if k == 27:
+		break
+cv2.destroyAllWindows()
+cap.release()
